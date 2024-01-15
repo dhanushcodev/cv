05719fca4974c455609d7f0191099c8d227bfd41
Dhanush kumar k <93399149+dhanushcodev@users.noreply.github.com>
2024-01-16 00:14:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,109 @@
+"use strict";
 
-var typed = new Typed('.text-animation', {
-    strings: ["Web Developer", "App Developer"],
-    typeSpeed: 40,
-    backSpeed: 40,
-    backDelay: 1000,
-    loop: true
+let loader = document.querySelector(".loader");
+
+function checkMediaQuery(query) {
+  return window.matchMedia(query).matches;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  animate();
+});
+
+if (checkMediaQuery("(max-width: 900px)")) {
+  let project_container = document.querySelector(".project-card-container");
+  project_container.classList.add("animateup");
+  let projcards = document.querySelectorAll("project-card");
+  for (let i = 0; i < projcards.length; i++) {
+    projcards[i].classList.remove("animateup");
+  }
+}
+
+function animate() {
+  const cards = document.getElementsByClassName("animateup");
+  const arr = Array.from(cards);
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const ele = entry.target;
+      if (entry.isIntersecting) {
+        ele.classList.add("animate-visible");
+        observer.unobserve(entry.target);
+      }
+    });
   });
 
+  arr.forEach((card) => {
+    observer.observe(card);
+  });
+}
 
-var curs = document.getElementById("cursor");
-document.addEventListener("mousemove",function(event){
-    curs.style.left = event.x+20 + "px";
-    curs.style.top = event.y+20 + "px";
-})
+
+
+let curs = document.getElementById("cursor");
+
+document.addEventListener("mousemove", function (event) {
+  curs.style.left = event.x + 20 + "px";
+  curs.style.top = event.y + 20 + "px";
+});
+
+let bottomnav = document.querySelector(".bottom-nav");
+let menu = document.querySelector(".menu");
+let close = document.querySelector(".close");
+let bottomnavcontent = document.querySelector(".bottom-nav-content-box");
+
+function slide() {
+  if (bottomnav.classList.contains("visible")) {
+    bottomnavcontent.classList.toggle("bottom-nav-visible");
+    menu.classList.toggle("menuinvisible");
+    close.classList.toggle("menuinvisible");
+    setTimeout(() => {
+      bottomnav.classList.toggle("visible");
+    }, 1000);
+    return;
+  }
+
+  bottomnav.classList.toggle("visible");
+  menu.classList.toggle("menuinvisible");
+  close.classList.toggle("menuinvisible");
+  bottomnavcontent.classList.toggle("bottom-nav-visible");
+}
+
+function scrollToEle(ele) {
+  const element = document.getElementById(ele);
+  element.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "start",
+  });
+}
+
+window.addEventListener("scroll", () => {
+  let scrollTop = document.querySelector(".arrow");
+  if (window.scrollY > 800) {
+    scrollTop.style.opacity = "1";
+  } else {
+    scrollTop.style.opacity = "0";
+  }
+});
+
+let typed = new Typed(".text-animation", {
+  strings: ["Web Developer", "App Developer"],
+  typeSpeed: 40,
+  backSpeed: 40,
+  backDelay: 1000,
+  loop: true,
+});
+
+window.addEventListener("load", function () {
+  loader.classList.add("disappear");
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && (event.key === "i" || event.key === "I")) {
+    event.preventDefault();
+  }
+});
+
+document.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+});
